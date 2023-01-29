49efd8187331a770d6649fdf71caaaf7d00c9a41
fah <phurichayakhem1@gmail.com>
2023-01-29 09:47:00 +0700

    responsive by Expanded

---
+++ layout/sizing/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo 1'),
+        ),
+        body: Center(
+          child: _buildRow(),
+        ),
+      ),
+    );
+  }
+}
+
+Widget _buildRow() => Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Image.asset('images/pic1.jpg'),
+        ),
+        Expanded(
+          child: Image.asset('images/pic2.jpg'),
+        ),
+        Expanded(
+          child: Image.asset('images/pic3.jpg'),
+        ),
+      ],
+    );
