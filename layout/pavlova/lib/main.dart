1db46afa313fd4314b8f9a9e8e1946f86d1d1041
fah <phurichayakhem1@gmail.com>
2023-01-29 10:11:54 +0700

    Nesting rows and columns

--- layout/pavlova/lib/main.dart
+++ layout/pavlova/lib/main.dart
@@ -15,7 +15,13 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: (Center(child: Container(child: star()))),
+        body: (Center(
+            child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+          child: Column(
+            children: [rating(), iconList()],
+          ),
+        ))),
       ),
     );
   }
@@ -30,4 +36,61 @@ Widget star() => Row(
         const Icon(Icons.star, color: Colors.black),
         const Icon(Icons.star, color: Colors.black),
       ],
+    );
+
+Widget rating() => Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          star(),
+          const Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+const descTextStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontFamily: 'Roboto',
+  letterSpacing: 0.5,
+  fontSize: 18,
+  height: 2,
+);
+
+Widget iconList() => Container(
+      padding: const EdgeInsets.all(20),
+      //EdgeInset = space around widget
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Column(
+          children: [
+            Icon(Icons.kitchen, color: Colors.green[500]),
+            const Text('PREP:'),
+            const Text('25 min'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.timer, color: Colors.green[500]),
+            const Text('COOK:'),
+            const Text('1 hr'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.restaurant, color: Colors.green[500]),
+            const Text('FEEDS:'),
+            const Text('4-6'),
+          ],
+        ),
+      ]),
     );
