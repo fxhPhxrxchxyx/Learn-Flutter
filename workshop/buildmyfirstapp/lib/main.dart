886f792a601037eea996c2f40e211f4096ee4ed1
fah <phurichayakhem1@gmail.com>
2023-02-01 02:24:48 +0700

    Center the UI

--- workshop/buildmyfirstapp/lib/main.dart
+++ workshop/buildmyfirstapp/lib/main.dart
@@ -51,17 +51,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: const Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('A random AWESOME idea:'),
+            BigCard(pair: pair),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: const Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -86,7 +89,11 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase, style: style),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: pair.asPascalCase,
+        ),
       ),
     );
   }
