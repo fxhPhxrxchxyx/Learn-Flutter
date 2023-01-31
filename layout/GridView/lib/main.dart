81921b3d3701bd4177ec3b73b57d9ee600704226
fah <phurichayakhem1@gmail.com>
2023-01-31 22:43:20 +0700

    list view & use static const

--- layout/GridView/lib/main.dart
+++ layout/GridView/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
   runApp(const MyApp());
@@ -7,14 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const showGrid = false; // Set to false to show ListView
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Grid and list',
-      home: Scaffold(
-          body: Container(
-        child: _buildGrid(),
-      )),
+      home:
+          Scaffold(body: Center(child: showGrid ? _buildGrid() : _buildList())),
     );
   }
 }
@@ -28,3 +28,38 @@ Widget _buildGrid() => GridView.extent(
 
 List<Container> _buildGridTileList(int count) => List.generate(
     count, (i) => Container(child: Image.asset('images/pic$i.jpg')));
+
+// ListView
+Widget _buildList() {
+  return ListView(
+    children: [
+      _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+      _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+      _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+      _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+      _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+          Icons.theaters),
+      _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+      const Divider(),
+      _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+      _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+      _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+      _tile('La Ciccia', '291 30th St', Icons.restaurant),
+    ],
+  );
+}
+
+ListTile _tile(String title, String subtitle, IconData icon) {
+  return ListTile(
+    title: Text(title,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),
+    subtitle: Text(subtitle),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500],
+    ),
+  );
+}
