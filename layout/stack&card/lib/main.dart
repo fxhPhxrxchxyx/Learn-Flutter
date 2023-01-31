f4d874030954e552cb27411affae73bed375ce52
fah <phurichayakhem1@gmail.com>
2023-02-01 00:48:40 +0700

    card with Divider

--- layout/stack&card/lib/main.dart
+++ layout/stack&card/lib/main.dart
@@ -7,12 +7,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const showCard = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Stack and Card',
       home: Scaffold(
-        body: Center(child: _buildStack()),
+        body: Center(child: showCard ? _buildCard() : _buildStack()),
       ),
     );
   }
@@ -37,5 +39,48 @@ Widget _buildStack() {
         ),
       )
     ],
+  );
+}
+
+Widget _buildCard() {
+  return SizedBox(
+    height: 210,
+    child: Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: const Text(
+              '1625 Main Street',
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+            subtitle: const Text('My City, CA 99984'),
+            leading: Icon(
+              Icons.restaurant_menu,
+              color: Colors.blue[500],
+            ),
+          ),
+          //line to divide section
+          const Divider(),
+
+          ListTile(
+            title: const Text(
+              '(408) 555-1212',
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+            leading: Icon(
+              Icons.contact_phone,
+              color: Colors.blue[500],
+            ),
+          ),
+          ListTile(
+            title: const Text('costa@example.com'),
+            leading: Icon(
+              Icons.contact_mail,
+              color: Colors.blue[500],
+            ),
+          ),
+        ],
+      ),
+    ),
   );
 }
