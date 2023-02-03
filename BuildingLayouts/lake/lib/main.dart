bbdbb88c8751e9e60fa21ed620ef95790a3aa15d
fah <phurichayakhem1@gmail.com>
2023-02-03 09:29:21 +0700

    Adding interactive fav widget

--- BuildingLayouts/lake/lib/main.dart
+++ BuildingLayouts/lake/lib/main.dart
@@ -50,11 +50,12 @@ Widget titleSection = Container(
       ),
       Text('Kandersteg, Switzerland', style: TextStyle(color: Colors.grey[500]))
     ])),
-    Icon(
-      Icons.star,
-      color: Colors.red[500],
-    ),
-    const Text('41'),
+    // Icon(
+    //   Icons.star,
+    //   color: Colors.red[500],
+    // ),
+    // const Text('41'),
+    FavoriteWidget(),
   ]),
 );
 
@@ -105,3 +106,51 @@ Widget textSection = const Padding(
 
 //Implement the image section
 //add ListView for responsive pic cover
+
+//Adding interactivity
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+}
