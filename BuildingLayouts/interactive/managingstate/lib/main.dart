eddd8ee5eead0e1ec6171b0d89d40fc9cff62cf2
fah <phurichayakhem1@gmail.com>
2023-02-06 15:47:03 +0700

    Managing state (state full)

---
+++ BuildingLayouts/interactive/managingstate/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter state Demo')),
+        body: const Center(
+          child: TapboxA(),
+        ),
+      ),
+    );
+  }
+}
+
+//state
+class TapboxA extends StatefulWidget {
+  const TapboxA({super.key});
+
+  @override
+  State<TapboxA> createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false; // variable to set the state
+
+  //function to handle tap set state _active to opposite of current state
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //GestureDetector() is default from flutter
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
