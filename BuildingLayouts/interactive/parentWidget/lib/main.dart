c3a4b2c10ad98da3c238a910532af938e29d5069
fah <phurichayakhem1@gmail.com>
2023-02-06 16:55:45 +0700

    parent widget

---
+++ BuildingLayouts/interactive/parentWidget/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter state Demo')),
+        body: const Center(
+          child: _ParentWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class _ParentWidget extends StatefulWidget {
+  const _ParentWidget({super.key});
+
+  @override
+  State<_ParentWidget> createState() => __ParentWidgetState();
+}
+
+class __ParentWidgetState extends State<_ParentWidget> {
+  bool _active = false;
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapBoxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  const TapboxB({super.key, this.active = false, required this.onChanged});
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
